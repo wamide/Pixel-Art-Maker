012e72e7b53438fa6e0222396ad1c9a0d3a31b40
Wamide Egunjobi <40127262+wamide@users.noreply.github.com>
2018-07-01 23:05:57 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -2,9 +2,11 @@
 
 // Select size input
 
+//Define variables
 var height,width,color;
 
-// When size is submitted by the user, call makeGrid()
+
+// Submit event listener
 
 $('#sizePicker').submit(function (event){
 
@@ -16,26 +18,34 @@ $('#sizePicker').submit(function (event){
 
 	makeGrid(height,width);
 
-	//console.log('Height: ' + height + ' and width: ' + width);
 })
 
+// When size is submitted by the user, call makeGrid()
+
 function makeGrid(x, y) {
+
 	$('tr').remove();
-	for (var a = 1; a<=i; a++){
-		$('#pixelCanvas').append('<tr id=table' + a + '></tr>');
-		for (var b =1; b<=j; b++){
-			$('#table' + a).append('<td></td>');
+
+	for (var i = 1; i<=x; i++){
+
+		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+
+		for (var j =1; j<=y; j++){
+
+			$('#table' + i).append('<td></td>');
+
 		}
 	}
 	$('td').click(function addColor(){
 		color = $('#colorPicker').val();
-		if ($(this).attr('style')){
 
+		if ($(this).attr('style')){
 			$(this).removeAttr('style')
+
 		}
-    else
-    {
-			$(this).attr('style','background-color:' + color);
+    else {
+		$(this).attr('style','background-color:' + color);
 		}
 	})
+
 }
